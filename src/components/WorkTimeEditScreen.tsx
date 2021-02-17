77fecd7dd50699f4c8399e6469d77bb8e1bf7ee8
Aki Wiksten <aki.wiksten@medixine.com>
2021-02-17 19:04:14 +0200

    Various tasks.

--- src/components/WorkTimeEditScreen.tsx
+++ src/components/WorkTimeEditScreen.tsx
@@ -10,6 +10,7 @@ import {
   ScrollView,
   Picker,
   Button,
+  Alert,
 } from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
@@ -22,7 +23,6 @@ type WsProps = {
 const WorkTimeEditScreen: React.FC<WsProps> = (props) => {
   const [date, setDate] = useState('9:00');
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [value, onChangeText] = React.useState('Useless Placeholder');
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -42,15 +42,16 @@ const WorkTimeEditScreen: React.FC<WsProps> = (props) => {
     showDatePicker();
   };
 
-  return (
-    <Fragment>
-      <Text style={styles.date}>Selected Day: {props.selectedDate}</Text>
-      <Text style={{textAlign: 'center'}}>
-        Every field has at least a time in hours and minutes.
-      </Text>
-      <ScrollView
-        contentContainerStyle={[styles.container, styles.childContainer]}>
-        <Text style={styles.item} onPress={onPressTitle}>
+  const TimeEnum = Object.freeze({
+    beginTime: 1,
+    endTime: 2,
+    dailyWorkEstimate: 3,
+  });
+
+  const MyDatePicker = (props) => {
+    return (
+      <Fragment>
+        <Text style={styles.myDatePickerText} onPress={onPressTitle}>
           Begin Time: {date}
         </Text>
         <DateTimePickerModal
@@ -59,25 +60,84 @@ const WorkTimeEditScreen: React.FC<WsProps> = (props) => {
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
         />
-        <Text style={styles.item} onPress={onPressTitle}>
-          End Time: {date}
-        </Text>
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="time"
-          onConfirm={handleConfirm}
-          onCancel={hideDatePicker}
-        />
-        <Text style={styles.item} onPress={onPressTitle}>
-          Daily Work Estimate: {date}
-        </Text>
-        <TextInput onChangeText={(text) => onChangeText(text)} value={value} />
+      </Fragment>
+    );
+  };
+
+  const MyTextInput = (props) => {
+    const {control, handleSubmit, errors} = useForm();
+    console.log('errors', errors);
+    const onSubmit = (d) => Alert.alert('Form Data', JSON.stringify(d));
+    return (
+      <View style={styles.myTextInputView}>
+        <Text style={styles.myTextInputText}>Daily Work Estimate:</Text>
+        <View>
+          <Controller
+            control={control}
+            render={({onChange, onBlur, value}) => (
+              <TextInput
+                autoFocus={true}
+                style={styles.myTextInput}
+                onBlur={onBlur}
+                onChangeText={(text) => onChange(text)}
+                value={value}
+                placeholder="Time"
+                onSubmitEditing={handleSubmit(onSubmit)}
+              />
+            )}
+            name="dweName"
+            rules={{
+              required: true,
+              minLength: 1,
+              pattern: /^(10|11|12|[1-9]):[0-5][0-9]$/,
+            }}
+            defaultValue=""
+          />
+          {errors.dweName && <Text>Estimate is required.</Text>}
+          <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+        </View>
+      </View>
+    );
+  };
+  return (
+    <Fragment>
+      <Text style={styles.date}>Selected Day: {props.selectedDate}</Text>
+      <Text style={{textAlign: 'center'}}>
+        Every field has at least a time in hours and minutes.
+      </Text>
+      <ScrollView contentContainerStyle={[styles.container]}>
+        <MyDatePicker time={TimeEnum.beginTime} />
+        <MyDatePicker time={TimeEnum.endTime} />
+        <MyTextInput time={TimeEnum.dailyWorkEstimate} />
       </ScrollView>
     </Fragment>
   );
 };
 
 const styles = StyleSheet.create({
+  myTextInputView: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: 'black',
+    flexDirection: 'row',
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  myTextInput: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  myTextInputText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    height: '100%',
+    padding: 10,
+  },
   flextime: {
     flexDirection: 'row',
   },
@@ -110,17 +170,14 @@ const styles = StyleSheet.create({
   parentContainer: {
     flexDirection: 'column',
   },
-  childContainer: {
-    flexDirection: 'column',
-  },
   container: {
     justifyContent: 'flex-start',
     alignItems: 'flex-start',
     paddingTop: 22,
     marginBottom: 100,
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
-  item: {
+  myDatePickerText: {
     padding: 10,
     fontSize: 18,
     fontWeight: 'bold',
@@ -143,9 +200,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 18,
     marginTop: 12,
-  },
-  textInput: {
-    fontSize: 14,
   },
   details: {
     justifyContent: 'center',
